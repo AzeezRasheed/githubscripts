5309f23b98f8c2dca150e7390d39e377ca8d85b5
Abdulazeez Abdulrasheed <rayshmanazeez@gmail.com>
2023-05-31 18:17:33 +0100

    test-stage

--- inactive-users.mjs
+++ inactive-users.mjs
@@ -6,6 +6,7 @@ import cron from "node-cron";
 import fs from "fs";
 import csv from "csv-parser";
 import { createObjectCsvWriter } from "csv-writer";
+import axios, { all } from "axios";
 import nodemailer from "nodemailer";
 
 dotenv.config();
@@ -13,9 +14,9 @@ const app = express();
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Server listening on port: ${port}..`));
 
-const send_to = process.env.EMAIL_ORGANIZATION; // this is the organisation email that we want to send the users to
-const sent_from = process.env.EMAIL_USER; // this is the organisation email that we want to send the users to
-const reply_to = process.env.EMAIL_USER; // this is the email address that in charge of deleting the users
+const send_to = process.env.EMAIL_ORGANIZATION;
+const sent_from = process.env.EMAIL_USER;
+const reply_to = process.env.EMAIL_USER;
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -27,35 +28,25 @@ const token = process.env.GITHUB_API_TOKEN;
 const inactiveUsers_filename = "inactive-users.csv";
 const deletedUsers_filename = "deleted-users.csv";
 
-const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
-  // Create Email Transporter
-  const transporter = nodemailer.createTransport({
-    host: "smpt.gmail.com",
-    secure: false, // Set it to true if you want to use TLS
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+function checkIfLoggedIn30DaysAgo(givenDate) {
+  // Convert the given date string to a Date object
+  const givenDateObj = new Date(givenDate);
 
-  // Option for sending email
-  const options = {
-    from: sent_from,
-    to: send_to,
-    replyTo: reply_to,
-    subject: subject,
-    html: message,
-  };
+  // Get the current date
+  const currentDate = new Date();
 
-  // send email
-  transporter.sendMail(options, function (err, info) {
-    if (err) {
-      console.log(err);
-    } else {
-      //   console.log(info);
-    }
-  });
-};
+  // Calculate the date 30 days ago
+  const thirtyDaysAgo = new Date(
+    currentDate.getTime() - 30 * 24 * 60 * 60 * 1000
+  );
+
+  // Check if the given date is within the last 30 days
+  if (givenDateObj >= thirtyDaysAgo && givenDateObj <= currentDate) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 function getInactiveUsers(users, days = 30) {
   const filteredUsers = users.filter((users) => {
@@ -110,135 +101,209 @@ async function writeCsvFile(path, header, data) {
   }
 }
 
-async function inactiveUsersIn30Days() {
-  let allUsers = [];
+async function fetchProcessedUsersTest() {
+  try {
+    const data = await new Promise((resolve, reject) => {
+      fs.readFile("usersList.json", "utf8", (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      });
+    });
 
+    const allUsers = JSON.parse(data);
+    return allUsers;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
+
+async function fetchAllUsers() {
+  let allUsers = [];
   let page = 1;
   let hasNextPage = true;
-  try {
-    // Get the list of users
-    const users_url = `https://api.github.com/orgs/${orgName}/members`;
-    while (hasNextPage) {
-      const url = `${users_url}?per_page=100&page=${page}`;
-      const response = await fetch(url, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+  // Get the list of users
+  const users_url = `https://api.github.com/orgs/${orgName}/members`;
+  while (hasNextPage) {
+    console.log(`processing batch ${page} of all users`);
 
-      if (response.ok) {
-        const users = await response.json();
-        allUsers = allUsers.concat(users);
-
-        const linkHeader = response.headers.get("Link");
-        hasNextPage = linkHeader && linkHeader.includes('rel="next"');
-        page++;
-      } else {
-        console.error("Error retrieving users:", response.status);
-        break;
-      }
-    }
-    // console.log(users, "------");
-    console.log("Total users:", allUsers.length);
-
-    const csvData = allUsers
-      .map((user) => `${user.id},${user.login},${user.url}`)
-      .join("\n");
-
-    fs.writeFile("usersList.csv", csvData, (err) => {
-      if (err) {
-        console.error("Error writing to CSV file:", err);
-      } else {
-        console.log("Data written to CSV file successfully.");
-      }
+    const url = `${users_url}?per_page=100&page=${page}`;
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     });
-    //   Get the last login time of each users
-    const usersWithLastLogin = await Promise.all(
-      allUsers.map(async (users) => {
-        const userResponse = await fetch(users.url, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        const userData = await userResponse.json();
-        return {
-          login: users.login,
-          id: users.id,
-          url: users.url,
-          lastLogin: userData.updated_at,
-        };
-      })
+
+    if (response.ok) {
+      const users = await response.json();
+      allUsers = allUsers.concat(users);
+
+      const linkHeader = response.headers.get("Link");
+      hasNextPage = linkHeader && linkHeader.includes('rel="next"');
+      page++;
+    } else {
+      console.error("Error retrieving user:", response.status);
+      console.log(response);
+      break;
+    }
+  }
+
+  console.log("Total users:", allUsers.length);
+  return allUsers;
+}
+
+async function inactiveUsersIn30Days() {
+  try {
+    // let allUsers = await fetchAllUsers();
+
+    // const csvData = allUsers
+    // 	.map((user) => `${user.id},${user.login},${user.url}`)
+    // 	.join("\n");
+
+    // fs.writeFile("usersList.csv", csvData, (err) => {
+    // 	if (err) {
+    // 		console.error("Error writing to CSV file:", err);
+    // 	} else {
+    // 		console.log("Data written to CSV file successfully.");
+    // 	}
+    // });
+
+    // // Get the last login time of each users
+    // console.log(`Fetching users details...`)
+    // const usersWithLastLogin = await Promise.all(
+    // 	allUsers.slice(0, 220).map(async (user) => {
+    // 		const userResponse = await fetch(user.url, {
+    // 			headers: {
+    // 				Authorization: `Bearer ${token}`,
+    // 			},
+    // 		});
+    // 		const userData = await userResponse.json();
+    // 		console.log(user.id)
+    // 		return {
+    // 			login: user.login,
+    // 			id: user.id,
+    // 			url: user.url,
+    // 			lastLogin: userData.updated_at,
+    // 		};
+    // 	})
+    // )
+
+    // console.log("usersWithLastLogin", usersWithLastLogin.length)
+    // fs.writeFile("usersList.json", JSON.stringify(usersWithLastLogin), (err) => {
+    // 	if (err) {
+    // 		console.error("Error writing usersList to CSV file:", err);
+    // 	} else {
+    // 		console.log("usersList written to CSV file successfully.");
+    // 	}
+    // });
+
+    // Example usage to check if a user have logged in within the last 30 days:
+    const givenDate = "2022-06-09T15:39:10Z";
+    const isLoggedIn30DaysAgo = checkIfLoggedIn30DaysAgo(givenDate);
+
+    console.log(
+      `The user ${
+        isLoggedIn30DaysAgo ? "logged in" : "did not log in"
+      } 30 days ago`
     );
 
-    const inactiveUsers = getInactiveUsers(usersWithLastLogin);
+    console.log(`Processing inactive users..`);
 
-    // Create the email message
-    const subject = "Inactive Users Report";
-    const message = `
-	<html>
-	  <head>
-		<style>
-		  table {
-			border-collapse: collapse;
-			width: 100%;
-		  }
-  
-		  th, td {
-			padding: 8px;
-			text-align: left;
-			border-bottom: 1px solid #ddd;
-		  }
-  
-		  th {
-			background-color: #f2f2f2;
-		  }
-		</style>
-	  </head>
-	  <body>
-		<h2>Here are the users who have not logged in to ${orgName} in the last 30 days:</h2>
-		<table>
-		  <tr>
-			<th>Login</th>
-			<th>ID</th>
-			<th>URL</th>
-			<th>Last Login</th>
-		  </tr>
-		  ${inactiveUsers
-        .map(
-          (user) => `
-				<tr>
-				  <td>${user.login}</td>
-				  <td>${user.id}</td>
-				  <td>${user.url}</td>
-				  <td>${user.lastLogin}</td>
-				</tr>
-			  `
-        )
-        .join("")}
-		</table>
-	  </body>
-	</html>
-  `;
+    const usersWithLastLogin = await fetchProcessedUsersTest();
 
-    try {
-      await sendEmail(subject, message, send_to, sent_from, reply_to);
-      console.log(`Report sent to ${process.env.EMAIL_ORGANIZATION}`);
-    } catch (error) {
-      console.log("Email not sent, please try again");
-    }
-
+    let inactiveUsers = getInactiveUsers(usersWithLastLogin);
     console.log(
       `${inactiveUsers.length} users have not logged in to ${orgName} in the last 30 days`
     );
 
+    // number users
+    inactiveUsers = inactiveUsers.map((user, i) => {
+      return {
+        "s/n": i + 1,
+        login: user.login,
+        id: user.id,
+        url: user.url,
+        lastLogin: user.lastLogin,
+      };
+    });
+
     // write inactiveUsers users to a CSV file
     const header = [
+      { id: "s/n", title: "s/n" },
       { id: "login", title: "login" },
       { id: "id", title: "id" },
       { id: "url", title: "url" },
       { id: "lastLogin", title: "lastLogin" },
     ];
+
     await writeCsvFile(inactiveUsers_filename, header, inactiveUsers);
+
+    // Set the batch size
+    const batchSize = 100;
+
+    // Calculate the number of iterations required
+    const iterations = Math.ceil(inactiveUsers.length / batchSize);
+
+    // Process sending the inactiveUsers to the teams channel in batches
+    for (let i = 0; i < iterations; i++) {
+      // Calculate the start and end index for the current batch
+      const start = i * batchSize;
+      const end = start + batchSize;
+
+      // Get the current batch of inactiveUsers using array slicing
+      const currentBatch = inactiveUsers.slice(start, end);
+      // Create the email message
+      // const subject = `${inactiveUsers.length} Users have not logged in to ${orgName} github account in the last 30 days`;
+      const date = new Date();
+
+      let day = date.getDate();
+      let month = date.getMonth() + 1;
+      let year = date.getFullYear();
+
+      // This arrangement can be altered based on how we want the date's format to appear.
+      let currentDate = `${day}-${month}-${year}`;
+
+      const message = `<html><head><style>table{border-collapse:collapse;width:100%}th,td{padding:8px;text-align:left;border-bottom:1px solid #ddd}th{background-color:#f2f2f2}</style></head><body><h1>Report for <code>${currentDate}</code></h1><h2>${
+        inactiveUsers.length
+      } Users have not logged in to ${orgName} github account in the last 30 days</h2><h3>Here are the list of users ${
+        start + 1
+      } - ${end}</h3><br/><br/><table><tr><th>s/n</th><th>Login</th><th>ID</th><th>URL</th><th>Last Login</th></tr>${currentBatch
+        .map(
+          (user) =>
+            `<tr><td>${user["s/n"]}</td><td>${user.login}</td><td>${user.id}</td><td>${user.url}</td><td>${user.lastLogin}</td></tr>`
+        )
+        .join("")}</table></body></html>`;
+
+      const webhookUrl =
+        "https://sysco.webhook.office.com/webhookb2/955026a7-0e71-4bbd-8bd1-fa20c236add1@b7aa4308-bf33-414f-9971-6e0c972cbe5d/IncomingWebhook/bec292168bb1416693f8b99cbdc3dd23/9e05cd3f-c133-4abc-a905-5368feb8deae";
+      const messagePayload = {
+        "@type": "MessageCard",
+        "@context": "http://schema.org/extensions",
+        themeColor: "0072C6",
+        summary: "Inactive Users Report",
+        sections: [
+          {
+            activityTitle: "Inactive Users Report",
+            activitySubtitle: "Posted via webhook",
+            activityImage: "https://example.com/images/avatar.png",
+            text: message,
+            contenType: "html",
+          },
+        ],
+      };
+
+      axios
+        .post(webhookUrl, messagePayload)
+        .then((response) => {
+          console.log("Webhook message sent:", response.data);
+        })
+        .catch((error) => {
+          console.error("Error sending webhook message:", error);
+        });
+    }
 
     // delete inactive users with prompt
     // The user is able to review and edit the generated csv for users marked for deletion at inactiveUsers_filename
@@ -268,13 +333,14 @@ async function inactiveUsersIn30Days() {
         })
         .on("end", () => {
           console.log(`InactiveUsers to be deleted ${inactiveUsers}`);
-          inactiveUsers.forEach((user) => {
-            deleteInactiveUser(user.login);
-            deletedUsers.push(user);
-          });
+          // inactiveUsers.forEach((user) => {
+          // 	deleteInactiveUser(user.login);
+          // 	deletedUsers.push(user);
+          // });
 
           // write deleted users users to a CSV file
           const header = [
+            { id: "s/n", title: "S/N" },
             { id: "login", title: "Login" },
             { id: "id", title: "ID" },
             { id: "url", title: "URL" },
